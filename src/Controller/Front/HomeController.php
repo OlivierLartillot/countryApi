70921a6fa23b23413f953b16f58ed103e87885c9
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-09-28 00:50:48 +0200

    ajout d'un menu utilisateur non connecté + gestion de boutons se connecter /se déconnecter

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_index")
      */
     public function index(): Response
     {
